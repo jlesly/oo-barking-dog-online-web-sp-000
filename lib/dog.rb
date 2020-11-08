f9fb022764fb156886520dfe059552ea7b5ade07
Lesly Juarez <leslyj2@gmail.com>
2020-11-08 17:47:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,11 @@
-class Dog 
-  def name =(name)
-    @name = name 
-  end 
- 
-  def name 
-    @name 
-  end 
-  
+  class Dog
+  def name= (name)
+    @name = name
+  end
+  def name
+    @name
+  end
   def bark
     puts "woof!"
-  end 
+  end
 end
